50ed3220c817d216407147f9c8cbf242732bc58b
EL Queso <66038123+el-queso@users.noreply.github.com>
2021-06-02 15:05:07 +0300

    Update settings.py

--- src/bubbling/settings.py
+++ src/bubbling/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd#zz!re=%_de0o%#0ute)=*o0)*6rwf%mw-d#b_!7$zst^%4=+'
+SECRET_KEY = '##################################################'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
